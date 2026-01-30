6d4df56c7f6c5548abacdd0274b0c1a85b1fb0e9
Balnur017 <b_izatulla@kbtu.kz>
2026-01-31 01:13:47 +0500

    Examples Commit

---
+++ Practice1/python-basics/DataTypes.py
@@ -0,0 +1,19 @@
+#Ex1
+x = 5
+print(type(x))
+
+#Ex2
+x = str("Hello World")
+print(type(x))
+
+#Ex3
+x = list(("apple", "banana", "cherry"))
+print(x)
+
+#Ex4
+x = complex(1j)
+print(type(x))
+
+#Ex5
+x = tuple(("apple", "banana", "cherry"))
+print(x)
